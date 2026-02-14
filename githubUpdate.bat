61e09c4f2f8ac2ac7c5bb20bd86cea55b6facd51
BG4mer <bostonking3235@gmail.com>
2026-02-14 18:53:32 +0000

    initial commit

---
+++ githubUpdate.bat
@@ -0,0 +1,19 @@
+@echo off
+
+title GitHub Update
+
+echo Add all changes to git
+
+git add .
+
+set /p commitMessage=Enter commit name: 
+
+echo Running git commit with the provided name...
+
+git commit -m "%commitMessage%"
+
+echo Pushing to the 'main' branch...
+
+git push origin main
+
+echo Finished!
